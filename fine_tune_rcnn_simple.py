b05611ff8bb1b298e81f7bc3c2ed9116e71cf491
dqminh2810 <dqminh2810@gmail.com>
2020-11-27 16:52:54 +0100

    update finetune and save model and evaluation

--- fine_tune_rcnn_simple.py
+++ fine_tune_rcnn_simple.py
@@ -32,7 +32,7 @@ args = vars(ap.parse_args())
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
 INIT_LR = 1e-4
-EPOCHS = 12
+EPOCHS = 20
 BS = 32
 nb_classes = 6
 
